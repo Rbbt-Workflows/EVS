1f8a7257caa3bcec8de8ec51d3dd2afdc88d72e8
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2020-01-30 02:12:53 +0100

    nil

--- lib/rbbt/sources/EVS.rb
+++ lib/rbbt/sources/EVS.rb
@@ -38,7 +38,44 @@ module EVS
     nil
   end
 
-  def self.database
+  EVS.claim EVS.mutations, :proc do |filename|
+    dumper = TSV::Dumper.new :key_field => "Genomic Mutation", :fields => ["MAF European American", "MAF African American", "MAF all"],
+      :type => :list, :cast => :to_f
+
+    dumper.init
+
+    files = EVS['.source'].glob("*.chr*.txt")
+
+    saver = Thread.new {
+      Misc.sensiblewrite(filename, dumper.stream)
+    }
+
+    TSV.traverse files.sort, :bar => "EVS files" do |file|
+      TSV.traverse file, :sep => " ", :bar => File.basename(file), :type => :list, :key_field => "base(NCBI.37)", :fields => ["Alleles", "MAFinPercent(EA/AA/All)"] do |chr_pos, values|
+        next if rsid == "none"
+
+        chr, position = chr_pos.split(":")
+        alleles, maf = values
+        mafs = maf.split("/")
+        alleles.split(";").each do |allele|
+          ref, alt = allele.split(">")
+          pos, alt = Misc.correct_vcf_mutation(position.to_i, ref, alt)
+          mutation = [chr, pos, alt] * ":"
+          dumper.add mutation, mafs
+        end
+      end
+    end
+
+    dumper.close
+    saver.join
+
+    nil
+  end
+  def self.database_rsid
     @@database ||= EVS.rsids.tsv :persist => true
+  end
+
+  def self.database
+    @@database ||= EVS.mutations.tsv :persist => true, :persist_engine => "BDB"
   end
 end
--- workflow.rb
+++ workflow.rb
@@ -8,16 +8,38 @@ Workflow.require_workflow "Genomes1000"
 module EVS
   extend Workflow
 
-  dep Genomes1000, :identify
-  task :annotate => :tsv do 
+  #dep Genomes1000, :identify
+  #task :annotate => :tsv do 
+  #  database = EVS.database_rsid
+  #  dumper = TSV::Dumper.new :key_field => "Genomic Mutation", :fields => database.fields, :type => :list, :cast => :to_f
+  #  dumper.init
+  #  TSV.traverse step(:identify), :into => dumper, :bar => "EVS" do |mutation,rsid|
+  #    mutation = mutation.first if Array === mutation
+  #    values = database[rsid] 
+  #    next if values.nil?
+  #    [mutation, values]
+  #  end
+  #end
+
+  input :mutations, :array, "Genomic Mutation", nil, :stream => true
+  input :by_position, :boolean, "Identify by position", false
+  task :annotate => :tsv do |mutations,by_position|
     database = EVS.database
-    dumper = TSV::Dumper.new :key_field => "Genomic Mutation", :fields => database.fields, :type => :list, :cast => :to_f
+    dumper = TSV::Dumper.new :key_field => "Genomic Mutation", :fields => database.fields, :type => (by_position ? :double : :list), :organism => EVS.organism
     dumper.init
-    TSV.traverse step(:identify), :into => dumper, :bar => "EVS" do |mutation,rsid|
-      mutation = mutation.first if Array === mutation
-      values = database[rsid] 
-      next if values.nil?
-      [mutation, values]
+    database.unnamed = true
+    TSV.traverse mutations, :type => :array, :into => dumper, :bar => self.progress_bar("Annotate EVS") do |mutation|
+      if by_position
+        position = mutation.split(":").values_at(0,1) * ":"
+        keys = database.prefix(position + ":")
+        next if keys.nil?
+        values = keys.collect{|key| database[key] }.uniq
+        [mutation, Misc.zip_fields(values)]
+      else
+        values = database[mutation]
+        next if values.nil?
+        [mutation, values]
+      end
     end
   end
 end
